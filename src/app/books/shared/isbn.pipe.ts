d9a8af5e622c736b9c366c9f62430f97a8d7753b
The Buildbot <buildbot@angular2buch.de>
2022-08-02 05:11:11 +0000

    Auto-generated commit [ci skip]
    Triggered by commit: https://github.com/angular-schule/buch/commit/242f66f246fe6f5a617571243b0e9f566f2ed655

--- src/app/books/shared/isbn.pipe.ts
+++ src/app/books/shared/isbn.pipe.ts
@@ -5,9 +5,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class IsbnPipe implements PipeTransform {
 
-  /*BS*/transform(value: string): string {
+  transform(value: string): string {
     if (!value) { return ''; }
     return `${value.substring(0, 3)}-${value.substring(3)}`;
-  }/*BE*/
+  }
 
 }
