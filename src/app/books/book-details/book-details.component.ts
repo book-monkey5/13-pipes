df08dd206dd8f9d0b2d56e1f81f84b7ba8aca340
The Buildbot <buildbot@angular2buch.de>
2022-08-09 17:43:28 +0000

    Auto-generated commit [ci skip]
    Triggered by commit: https://github.com/angular-schule/buch/commit/47394d9e722947f3a49635ebcf52290752a2d01c

--- src/app/books/book-details/book-details.component.ts
+++ src/app/books/book-details/book-details.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 import { BookStoreService } from '../../shared/book-store.service';
 import { Book } from '../../shared/book';
@@ -10,21 +11,20 @@ import { Book } from '../../shared/book';
   styleUrls: ['./book-details.component.css']
 })
 export class BookDetailsComponent {
-  book?: Book;
+  book$: Observable<Book>;
+
   constructor(
     private service: BookStoreService,
     private route: ActivatedRoute,
     private router: Router
   ) {
     const isbn = this.route.snapshot.paramMap.get('isbn')!;
-    this.service.getSingle(isbn).subscribe(book => {
-      this.book = book;
-    });
+    this.book$ = this.service.getSingle(isbn);
   }
 
-  removeBook() {
-    if (this.book && window.confirm('Remove book?')) {
-      this.service.remove(this.book.isbn).subscribe(() => {
+  removeBook(isbn: string) {
+    if (window.confirm('Remove book?')) {
+      this.service.remove(isbn).subscribe(() => {
         this.router.navigateByUrl('/books');
       });
     }
